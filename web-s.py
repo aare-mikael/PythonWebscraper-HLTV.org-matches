de264d8b5539e98c6fec209bef08e96b5ad6f2f4
aare-mikael <aaremikael@gmail.com>
2021-09-29 14:37:45 +0200

    First (hopefully) working file

--- web-s.py
+++ web-s.py
@@ -3,12 +3,13 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+
 dates=[]
 times=[]
 events=[]
+matchlengths=[]
 hometeams=[]
 awayteams=[]
-matchlengths=[]
 driver.get("https://www.hltv.org/matches")
 
 content = driver.page_source
@@ -16,4 +17,26 @@ soup = BeautifulSoup(content)
 for a in soup.findAll('a', href=True, attrs={'class':'upcomingMatchesSection'}):
     date=a.find('div', attrs={'class':'matchDayHeadline'})
     dates.append(date.text)
-    
+    for b in soup.findAll('b', href=True, attrs={'class':'upcomingMatch removeBackground' | 'upcomingMatch removeBackground oddRowBgColor'}):
+        time=b.find('div', attrs={'class':'matchTime'})
+        event=b.find('div', attrs={'class':'matchEventName gtSmartphone-only'})
+        matchlength=b.find('div', attrs={'class':'matchMeta'})
+        hometeam=b.find('div', attrs={'class':'matchTeamName text-ellipsis'})
+        awayteam=b.find('div', attrs={'class':'matchTeamName text-ellipsis'})
+
+        times.append(time.text)
+        events.append(event.text)
+        matchlengths.append(matchlength.text)
+        hometeams.append(hometeam.text)
+        awayteams.append(awayteam.text)
+
+df = pd.DataFrame({
+    'Date' : dates, 
+    'Time' : times, 
+    'Event' : events, 
+    'Match length' : matchlengths, 
+    'Home team' : hometeams, 
+    'Away team' : awayteams
+    })
+
+df.to_excel(r'C:\Users\Ukjendt\OneDrive\Programmeringsprosjekter\Webscraper Python\Uthenta\Matches.xlsx', index = False, header=True)
