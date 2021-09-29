9e5be976cb5d43671d985f14485aa8ce39c4612f
aare-mikael <aaremikael@gmail.com>
2021-09-29 14:07:44 +0200

    from dankmemes to hltv.org/matches

--- web-s.py
+++ web-s.py
@@ -3,11 +3,17 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
-pics=[] # Memes that are pictures
-videos=[] # Memes that are gifs
-driver.get("https://www.reddit.com/r/dankmemes/")
+dates=[]
+times=[]
+events=[]
+hometeams=[]
+awayteams=[]
+matchlengths=[]
+driver.get("https://www.hltv.org/matches")
 
 content = driver.page_source
 soup = BeautifulSoup(content)
-for a in soup.findAll('a', href=True, attrs={'class':'_31qSD5'}):
-    fafafaf
+for a in soup.findAll('a', href=True, attrs={'class':'upcomingMatchesSection'}):
+    date=a.find('div', attrs={'class':'matchDayHeadline'})
+    dates.append(date.text)
+    
