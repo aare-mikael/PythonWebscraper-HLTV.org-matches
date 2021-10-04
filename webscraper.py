db849ac016e147c16764ec532b5da499fe841e7a
aare-mikael <aaremikael@gmail.com>
2021-10-04 12:38:28 +0200

    Improved look of output files

--- webscraper.py
+++ webscraper.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
+import xlwings as xw
 
 # Defining arrays to put the data in
 dates = []
@@ -54,13 +55,47 @@ df = pd.DataFrame({
     'Away team' : awayteams
     })
 
+# Closing chrome window opened by driver.get("https://www.hltv.org/matches")
+print("Closing Chrome window")
+driver.quit()
+
 # Writing CSV-files for easier use later
 print("Writing CSV file")
 df.to_csv('Uthenta/Matches.csv', encoding='utf-8')
 
-# Writing Excel-files for easier readability
-print("Writing Excel file")
-df.to_excel('Uthenta/Matches.xlsx', encoding='utf-8')
+wb = xw.Book()
 
-print("Closing Chrome window")
-driver.quit()
+sheet = wb.sheets["Ark1"] # I have Excel in norwegian, ark means sheet
+sheet.name = "Matches"
+sheet.api.Tab.Color = 0x70AD47
+
+sheet.range("A1").options(Index=False).value = df
+
+all_data_range = sheet.range("A1").expand('table')
+all_data_range.row_height = 30
+all_data_range.column_width = 18
+all_data_range.color = (240,240,240)
+all_data_range.api.Font.Name = 'Arial'
+all_data_range.api.Font.Size = 8
+all_data_range.api.HorizontalAlignment = xw.constants.HAlign.xlHAlignCenter
+all_data_range.api.VerticalAlignment = xw.constants.HAlign.xlHAlignCenter
+all_data_range.api.WrapText = True
+
+header_range = sheet.range("A1").expand('right')
+header_range.color = (125,255,100)
+header_range.api.Font.Color = 0xFFFFFF
+header_range.api.Font.Bold = True
+header_range.api.Font.Size = 9
+
+id_column_range = sheet.range("A2").expand('down')
+id_column_range.color=(198,224,180)
+
+data_ex_headers_range = sheet.range("A2").expand('table')
+
+for border_id in range(7,13):
+    data_ex_headers_range.api.Borders(border_id).Weight = 2
+    data_ex_headers_range.api.Borders(border_id).Color = 0xFFFFFF
+
+# Saving workbook to Excel
+print("Writing xlsx-file")
+wb.save(r"Uthenta\Matches.xlsx")
